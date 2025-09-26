38b791c501edd3bca6cac47c80010c2aa2b5b578
daniel <danielg9g1@gmail.com>
2025-09-26 16:48:22 -0600

    selololo

--- script.js
+++ script.js
@@ -4,12 +4,3 @@ const enlaces = document.querySelector("#nav-links")
 hamburguesa.addEventListener("click", () => {
     enlaces.classList.toggle("show")
 })
-
-// Submenú desplegable en móvil solo para "Rutas"
-const dropdown = document.querySelector('.dropdown');
-
-dropdown.addEventListener('click', () => {
-  if (window.innerWidth <= 768) {
-    dropdown.classList.toggle('active');
-  }
-});
